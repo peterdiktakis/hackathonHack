2d4219afd557a37ea3b20ab73d8b8ae7ad827b7e
Nico Plop <nyckoo@live.fr>
2015-05-22 21:15:40 -0400

    css test

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -5,7 +5,38 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<h1 class='text-center'>Welcome to our Travel App</h1>
-		</div>
+            <h3 class='text-center'>Travel deep into the Voodoo !</h3>
+        </div>
+        <div class="col-lg-4">
+            <div class="section">
+                <h3>Title</h3>
+                <p>text text text text text text text text text text text
+                    text text text text text text text text text text text
+                    text text text text text text text text text text text
+                    text text text text text text text text text text text</p>
+                <a href="#" class="btn-default">Button</a>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="section">
+                <h3>Title</h3>
+                <p>text text text text text text text text text text text
+                    text text text text text text text text text text text
+                    text text text text text text text text text text text
+                    text text text text text text text text text text text</p>
+                <a href="#" class="btn-default">Button</a>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="section">
+                <h3>Title</h3>
+                <p>text text text text text text text text text text text
+                    text text text text text text text text text text text
+                    text text text text text text text text text text text
+                    text text text text text text text text text text text</p>
+                <a href="#" class="btn-default">Button</a>
+            </div>
+        </div>
 	</div>
 </div>
 @endsection
