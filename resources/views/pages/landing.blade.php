2285706b40192c4440bff3d513c305faef4ca794
Nico Plop <nyckoo@live.fr>
2015-05-23 09:27:38 -0400

    Bagel commit

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <div class="background-test">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<h1 class='text-center'>Welcome to our Travel App</h1>
@@ -22,36 +23,15 @@
 
         </div>
         <div class="col-lg-4">
-            <div class="section">
-                <h3>Title</h3>
-                <p>text text text text text text text text text text text
-                    text text text text text text text text text text text
-                    text text text text text text text text text text text
-                    text text text text text text text text text text text</p>
-                <a href="#" class="btn-default">Button</a>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="section">
-                <h3>Title</h3>
-                <p>text text text text text text text text text text text
-                    text text text text text text text text text text text
-                    text text text text text text text text text text text
-                    text text text text text text text text text text text</p>
-                <a href="#" class="btn-default">Button</a>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="section">
-                <h3>Title</h3>
-                <p>text text text text text text text text text text text
-                    text text text text text text text text text text text
-                    text text text text text text text text text text text
-                    text text text text text text text text text text text</p>
-                <a href="#" class="btn-default">Button</a>
-            </div>
+            <h3 class="text-center">Title</h3>
+            <p>text text text text text text text text text text text
+                text text text text text text text text text text text
+                text text text text text text text text text text text
+                text text text text text text text text text text text</p>
+            <a href="#" class="btn-default">Button</a>
         </div>
 	</div>
+    </div>
 </div>
 @stop
 
