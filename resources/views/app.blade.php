260592cc23331804931e6da6fd874e35c3ca6f8e
Nico Plop <nyckoo@live.fr>
2015-05-22 20:31:24 -0400

    piter need help

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 	<title>Laravel</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('/less/Nico.less') }}" rel="stylesheet">
+    <link href="{{ asset('/css/nico.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
